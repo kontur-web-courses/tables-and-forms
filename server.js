50322ca64648fa0233fe53f9200568f025a2a579
Dmitriy2Belikov <44783389+Dmitriy2Belikov@users.noreply.github.com>
2021-01-09 03:17:58 +0500

    Tasks all

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const constructPageBody = require('./checkOrder');
+const constructPageBody = require("./checkOrder");
 
-
-const pageHead =
-`
+const pageHead = `
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -17,8 +15,7 @@ const pageHead =
 <h1>Результат заказа питомца</h1>
 `;
 
-const pageFoot =
-`
+const pageFoot = `
 <a href="/">⃪ Вернуться к форме заказа</a>
 </main>
 </body>
@@ -26,23 +23,21 @@ const pageFoot =
 `;
 
 app.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err);
-    }
+  if (err) {
+    return console.log("something bad happened", err);
+  }
 
-    console.log(`server is listening on ${port}`);
+  console.log(`server is listening on ${port}`);
 });
 
-
-app.use(express.static('static'));
+app.use(express.static("static"));
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/pets/orders', (request, response) => {
-    const reqBody = request.body;
-    const pageBody = constructPageBody(reqBody);
+app.post("/pets/orders", (request, response) => {
+  const reqBody = request.body;
+  const pageBody = constructPageBody(reqBody);
 
-    console.log(request.body);
+  console.log(request.body);
 
-    response.send(`${pageHead}${pageBody}${pageFoot}`);
+  response.send(`${pageHead}${pageBody}${pageFoot}`);
 });
-
